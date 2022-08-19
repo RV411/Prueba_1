277f1384525e56fda1e24c945280882842aaf98d
RV411 <ivan_rodriguez3000@hotmail.com>
2022-08-18 23:14:04 -0500

    new file:   css/reset.css new file:   css/style.css new file:   images/logo.svg new file:   images/not_found.png modified:   index.html modified:   index.js deleted:    style/style.css

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const ArrayVowel = ["a","e","i","o","u"];
 const ArrayChange=["ai","enter","imes","ober","ufat"];
 const ArrayReg=[/ai/mg,/enter/mg,/imes/mg,/ober/mg,/ufat/mg];
 
+const buttonCipher=document.querySelector("#buttonCipher");
+const buttonDecipher=document.querySelector("#buttonDecipher");
+const buttonCop=document.querySelector("#buttonCoppy");
+
 function Cipher(){
     const text=document.getElementById("textOld").value.toLowerCase();
+    if(text!=""){ document.getElementById("noResuelto").style.visibility = "hidden";}
+    else{ document.getElementById("noResuelto").style.visibility = "visible"; }
     let newText="";
     for(let x of text){
         if(x==ArrayVowel[0]){
@@ -20,21 +26,31 @@ function Cipher(){
         newText+=x;
     }
     document.getElementById("textNew").innerHTML=newText;
-    document.getElementById("buttonCoppy").style.display = '';
+    document.getElementById("buttonCoppy").style.visibility = 'visible';
+    document.getElementById("textOld").value = '';
 }
 
 function Decipher(){
     const text=document.getElementById("textOld").value.toLowerCase();
+    if(text!=""){ document.getElementById("noResuelto").style.visibility = "hidden";}
+    else{ document.getElementById("noResuelto").style.visibility = "visible"; }
+    
 	const atext = text.replace(ArrayReg[0], ArrayVowel[0]);
 	const etext = atext.replace(ArrayReg[1], ArrayVowel[1]);
 	const itext = etext.replace(ArrayReg[2], ArrayVowel[2]);
 	const otext = itext.replace(ArrayReg[3], ArrayVowel[3]);
 	const utext = otext.replace(ArrayReg[4], ArrayVowel[4]);
     document.getElementById("textNew").innerHTML=utext;
+    document.getElementById("buttonCoppy").style.visibility = 'visible';
+    document.getElementById("textOld").value = '';
+
 }
 
 function Copy() {
-    const text= document.querySelector("#textNew");
-    text.select();
-    document.execCommand("copy");
+    const text= document.querySelector("#textNew").value;
+    navigator.clipboard.writeText(text);
 }
+
+buttonCipher.onclick=Cipher;
+buttonDecipher.onclick=Decipher;
+buttonCop.onclick=Copy;
